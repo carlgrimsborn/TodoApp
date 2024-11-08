5bec2ce5e30df50f2735731cde4b1ff7031066c5
Carl Grimsborn <carlgrimsborn@gmail.com>
2024-11-08 16:41:10 +0100

    global css

---
+++ src/screens/AddTodo/AddTodo.jsx
@@ -0,0 +1,7 @@
+import '../../App.css';
+
+const AddTodo = () => {
+	return <div className='contentWrapper'>AddTodo</div>;
+};
+
+export default AddTodo;
---
+++ src/screens/ViewTodo/ViewTodo.jsx
@@ -0,0 +1,7 @@
+import '../../App.css';
+
+const ViewTodo = () => {
+	return <div className='contentWrapper'>ViewTodo</div>;
+};
+
+export default ViewTodo;
