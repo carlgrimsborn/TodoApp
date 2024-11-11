904a0e394df0f71dd8cc3d25117dece33f2bcd7c
Carl Grimsborn <carlgrimsborn@gmail.com>
2024-11-11 21:59:12 +0100

    edit note type useref submissison

--- src/components/EditNoteTypeForm/index.jsx
+++ src/components/EditNoteTypeForm/index.jsx
@@ -1,20 +1,23 @@
+import { useRef } from 'react';
 import './EditNoteTypeForm.css';
 
 const EditNoteTypeForm = () => {
+	const selectRef = useRef(null);
 	const onSubmit = (e) => {
-		console.log(e);
+		e.preventDefault();
+		console.log(selectRef.current.value);
 	};
 	return (
 		<form onSubmit={onSubmit}>
 			<label>
 				Change Status:
-				<select>
-					<option>Done</option>
-					<option>Prioritized</option>
-					<option>Delete</option>
+				<select ref={selectRef}>
+					<option value='done'>Done</option>
+					<option value='prioritized'>Prioritized</option>
+					<option value='deleted'>Delete</option>
 				</select>
 			</label>
-			<button type='submit' className='submit-note-type'>
+			<button type='submit' className='submit-note-type-btn'>
 				Submit
 			</button>
 		</form>
