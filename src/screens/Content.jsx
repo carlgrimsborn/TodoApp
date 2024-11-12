6efe23f6146148d1c99b71871c3547b18f0d2828
Carl Grimsborn <carlgrimsborn@gmail.com>
2024-11-12 13:16:45 +0100

    sorted notes

--- src/screens/Content.jsx
+++ src/screens/Content.jsx
@@ -11,11 +11,21 @@ const Content = () => {
 		oldNoteRef.current = note;
 		setNotes(newNotes);
 	};
-
+	const sortedNotes = notes.sort((a, b) => {
+		if (a.prioritized && !b.prioritized) {
+			return -1;
+		}
+		if (!a.prioritized && b.prioritized) {
+			return 1;
+		}
+		if (a.prioritized && b.prioritized) {
+			return 0;
+		}
+	});
 	return (
 		<div className='content'>
 			<AddNoteComponent addNote={AddNote} oldNote={oldNoteRef.current} />
-			<ViewNotesComponent notes={notes} />
+			<ViewNotesComponent notes={sortedNotes} />
 		</div>
 	);
 };
